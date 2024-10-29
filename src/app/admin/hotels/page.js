2e48a2022b585b874aea2f9a99e8b9bfb77bfe0e
alexandreSilva604 <xandlibra604@gmail.com>
2024-10-29 09:23:30 -0300

    Hotel page mostly done

--- src/app/admin/hotels/page.js
+++ src/app/admin/hotels/page.js
@@ -1,6 +1,58 @@
+import styles from '../../page.module.css';
+
 export default function AdminHotels() {
 
+    const hotels = [
+        {id: 1, name: 'Beach Hotel', location: 'Miami, FL'},
+        {id: 2, name: 'Oasis', location: 'Las Vegas, NV'},
+        {id: 3, name: 'Ye Olde Inn', location: 'New York, NY'}
+    ];
+
     return (
-        <h1>This is the hotels page.</h1>
+        <div className={styles.adminMain}>
+            <h1 className={styles.title}><i className='nav-icon bi bi-clipboard'></i> Hotels</h1>
+
+            <div className='card'>
+                <div className='card-header'>
+                    <h3 className='card-title'>Search Hotel</h3>
+                    <div className='card-tools'>
+                        <div className='input-group input-group-sm'>
+                            <input type='text' className='form-control float-right' placeholder='Enter a name or location...'
+                            style={{width: '65em'}} />
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className='card-body table-responsive p-0'>
+                    <table className='table table-hover text-nowrap'>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Location</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                hotels.map((hotel) => {
+                                    return (
+                                        <tr key={hotel.id}>
+                                            <td>{hotel.id}</td>
+                                            <td>{hotel.name}</td>
+                                            <td>{hotel.location}</td>
+                                            <td>
+                                                <button style={{marginRight: 5}} className='btn btn-primary'>Ban</button>
+                                                <button className='btn btn-danger'>Delete</button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     );
 }
