1fa6bb6afe6f74434e427eb3b21fdf99fa5341d9
alexandreSilva604 <xandlibra604@gmail.com>
2024-11-06 14:39:21 -0300

    Changed title to BookerSource.

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                 <meta name="description" content="" />
                 <meta name="author" content="" />
-                <title>Shop Homepage - Start Bootstrap Template</title>
+                <title>BookerSource</title>
                 <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
                 <link href="/css/styles.css" rel="stylesheet" />
