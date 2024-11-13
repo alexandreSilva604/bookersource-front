35d1a18f55ba1bcc9fd3b2da21a5a17abe49bedd
alexandreSilva604 <xandlibra604@gmail.com>
2024-11-13 14:25:48 -0300

    Added login and sign up options to top bar.

--- src/app/components/topbar.js
+++ src/app/components/topbar.js
@@ -9,9 +9,14 @@ export default function TopBar() {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li className="nav-item"><a className="nav-link" href="/" style={{color: '#22a'}}>Home</a></li>
-                        <li className="nav-item"><a className="nav-link" href="/hotels" style={{color: '#22a'}}>Hotels</a></li>
-                        <li className="nav-item"><a className="nav-link" href="/" style={{color: '#22a'}}>About</a></li>
+                        <li className="nav-item"><Link className="nav-link" href="/" style={{color: '#22a'}}>Home</Link></li>
+                        <li className="nav-item"><Link className="nav-link" href="/hotels" style={{color: '#22a'}}>Hotels</Link></li>
+                        <li className="nav-item"><Link className="nav-link" href="/" style={{color: '#22a'}}>About</Link></li>
+                    </ul>
+                    <ul className="navbar-nav align-items-center">
+                        <li className="nav-item"><Link className="nav-link" href="/login" style={{color: '#22a'}}>Login</Link></li>
+                        |
+                        <li className="nav-item"><Link className="nav-link" href="/signup" style={{color: '#22a'}}>Sign Up</Link></li>
                     </ul>
                 </div>
             </div>
