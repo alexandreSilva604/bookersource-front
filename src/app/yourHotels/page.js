5751f0a76991f0647e58127d2956b48fb2ac33d4
alexandreSilva604 <xandlibra604@gmail.com>
2025-02-03 14:42:11 -0300

    User hotels page now loads the registered hotels.

--- src/app/yourHotels/page.js
+++ src/app/yourHotels/page.js
@@ -10,24 +10,26 @@ export default function YourHotels() {
     const { user, setUser } = useContext(UserContext);
     const [yourHotels, setYourHotels] = useState([]);
 
+    const [loading, setLoading] = useState(true);
+
     function loadHotels() {
 
-        // CHANGE TO A FETCH LATER
-        setYourHotels([{
-            id: 1,
-            name: "Hotel 1",
-            country: "Brazil",
-            state: "São Paulo",
-            city: "São Paulo",
-            adminId: user.id
-        }, {
-            id: 2,
-            name: "Hotel 2",
-            country: "United States",
-            state: "New York",
-            city: "New York",
-            adminId: user.id
-        }]);
+        fetch(`http://localhost:8080/hotels/userHotels/${user.id}`, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'GET'
+        })
+        .then(r => {
+            return r.json();
+        })
+        .then(r => {
+            setYourHotels(r.hotels);
+            setLoading(false);
+        })
+        .catch(e => {
+            console.log(e.message);
+        });
     }
 
     useEffect(() => {
@@ -46,40 +48,56 @@ export default function YourHotels() {
                         </button>
                     </Link>
                 </div>
-                <div className="card-body">
-                    {
-                        yourHotels.size > 0
-                        ?
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>City</th>
-                                    <th>State</th>
-                                    <th>Country</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    yourHotels.map((hotel) => {
-                                        return (
-                                            <tr key={hotel.id}>
-                                                <th>{hotel.name}</th>
-                                                <th>{hotel.city}</th>
-                                                <th>{hotel.state}</th>
-                                                <th>{hotel.country}</th>
-                                            </tr>
-                                        );
-                                    })
-                                }
-                            </tbody>
-                        </table>
-                        :
-                        <div className="d-flex justify-content-center align-items-center mt-5 mb-5">
-                            <h3>You have no hotels registered (WIP).</h3>
-                        </div>
-                    }
-                </div>
+                {
+                    loading ?
+                    <p>Loading...</p>
+                    :
+                    <div className="card-body">
+                        {
+                            yourHotels && yourHotels.length > 0
+                            ?
+                            <table className="table table-hover text-center">
+                                <thead>
+                                    <tr>
+                                        <th scope="row">Id</th>
+                                        <th scope="row">Name</th>
+                                        <th scope="row">City</th>
+                                        <th scope="row">State</th>
+                                        <th scope="row">Country</th>
+                                        <th scope="row">Management</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        yourHotels.map((hotel) => {
+                                            return (
+                                                <tr key={hotel.id}>
+                                                    <th scope="row">{hotel.id}</th>
+                                                    <td>{hotel.name}</td>
+                                                    <td>{hotel.city}</td>
+                                                    <td>{hotel.state}</td>
+                                                    <td>{hotel.country}</td>
+                                                    <td>
+                                                        <Link href={`/manageRooms/${hotel.id}`}>
+                                                            <button className="btn btn-primary"
+                                                            style={{backgroundColor: '#22a', border: 'none'}}>
+                                                                Manage Rooms
+                                                            </button>
+                                                        </Link>
+                                                    </td>
+                                                </tr>
+                                            );
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                            :
+                            <div className="d-flex justify-content-center align-items-center mt-5 mb-5">
+                                <h3>You have no hotels registered.</h3>
+                            </div>
+                        }
+                    </div>
+                }
             </div>
         </div>
     );
